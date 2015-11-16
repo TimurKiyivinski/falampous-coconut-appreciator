c1ecc7a27f1e9ab935481f79f7c6bdddab783819
Timur Kiyivinski <deathseclipse@gmail.com>
2015-11-16 17:34:12 +0800

    Complete server-client aspects.

--- client.py
+++ client.py
@@ -1,14 +1,21 @@
 #!/bin/env python3
 import game
-import argparse
+import argparse, time
+from multiprocessing import Process
 
 def main(args):
-    while True:
-        try:
-            client = game.ClientState()
-            client.start(args.server, args.port)
-        except KeyboardInterrupt:
-            return False
+    try:
+        client = game.ClientState()
+        client.connect(args.server, args.port)
+        listener = Process(target=client.listen)
+        listener.start()
+        print('Waiting 10 seconds to connect.')
+        time.sleep(10)
+        print('Connect.')
+        client.start()
+        listener.join()
+    except KeyboardInterrupt:
+        return False
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Medieval Battle Chess client application.')
--- game.py
+++ game.py
@@ -1,30 +1,42 @@
 from stockings import stockings
-import pickle
-from multiprocessing import Process, Queue
+import pickle, random, string, datetime
+from multiprocessing import Process
 
 class Token:
-    def __init__(self, name = 'Null', token = 'N', attacks = []):
+    def __init__(self, name = 'Null', token = 'N', attacks = {}):
         self.name = name
         self.token = token
         self.attacks = attacks
-        pass
-    def owner(self, player):
-        self.player = player.id
+    def player(self, player):
+        self.player = player
     def attack(self, token):
         pass
 
 class Board:
     def __init__(self, width = 8, height = 8):
-        self.tokens = [[[] for i in range(width)] for ii in range(height)]
-        pass
+        self.tokens = [[0 for i in range(width)] for ii in range(height)]
+        self.width = width
+        self.height = height
     def move(self, origin, destination):
         pass
-    def tokens(self, tokens):
-        #TODO: Create a token start function
+    def rand(self):
+        """
+        Return a random pair of coordinates.
+        """
+        return random.randint(0, self.height - 1), random.randint(0, self.width - 1)
+    def token(self, tokens, player):
+        for i in tokens:
+            for ii in range(int(i['count'])):
+                token = Token(i['name'], i['token'], i['hit'])
+                token.player(player)
+                x, y = self.rand()
+                while not self.tokens[x][y] == 0:
+                    x, y = self.rand()
+                self.tokens[x][y] = token
 
 class Player:
     def __init__(self):
-        pass
+        self.id = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase) for i in range(32))
 
 class Input:
     def __init__(self):
@@ -56,10 +68,10 @@ class GameState(State):
         self.server = stockings.SocketServer(int(self.config['port']), 4096)
         # Create game board
         self.board = Board(int(self.config['board']['width']), int(self.config['board']['height']))
-        # Create board tokens
-        # self.board.tokens(self.config['tokens'])
         # Create empty connection list
         self.connections = []
+        # Has game started
+        self.started = False
 
         # Start server
         try:
@@ -74,63 +86,103 @@ class GameState(State):
         # Handle new connection
         if data.new:
             if len(self.connections) < 2:
-                print('Connecting client.')
-                self.connections.append(data)
-                #TODO: Return confirmation ClientState
-                print('Done')
+                try:
+                    print('Connecting client %i.' % (len(self.connections) + 1))
+                    client = ClientState.clone(data)
+                    self.board.token(self.config['tokens'], client.player)
+                    client.board = self.board
+                    self.connections.append(client)
+                    if len(self.connections) == 2:
+                        print('Setting turn')
+                        client.turn = self.connections[0].player.id
+                except Exception as e:
+                    print(e)
+                print('Returning connection.')
+                return pickle.dumps(client)
             else:
                 print('Session overpopulated.')
-                #TODO: Return failed ClientState
+                return pickle.dumps(ClientState.message('Overpopulated'))
         # Handle closing session
         elif data.close:
             print('Closing client session.')
             self.server.close()
-        # Handle normal request
         else:
-            print('Player %s says: %s' % data.name, "Cat")
-            self.server.broadcast(data.copy())
-            #TODO: Return game ClientState
+            print('Normal operation')
+            try:
+                client = ClientState.clone(data)
+                if data.turn == self.connections[0].player.id:
+                    print('%s to %s' % (data.turn, self.connections[0].player.id))
+                    client.turn = self.connections[1].player.id
+                else:
+                    print('%s to %s' % (data.turn, self.connections[1].player.id))
+                    client.turn = self.connections[0].player.id
+                return pickle.dumps(client)
+            except Exception as e:
+                print(e)
 
 class ClientState(State):
     def __init__(self):
         State.__init__(self)
-        self.new = True
+        self.new = False
         self.close = False
-    def interface(self, view, input):
+        self.over = False
+        self.text = 'Hello'
+        self.turn = 0
+        self.player = Player()
+    def message(self, message = ''):
         """
-        Override view and input classes.
-        This is possible if function compatibility is maintained.
-        See help() for more information.
+        Send a plain ClientState for the purpose of sending a message.
         """
-        pass
-    def copy(self, message = ''):
         clean = ClientState()
         clean.new = message == 'New'
         clean.close = message == 'Close'
+        clean.over = message == 'Overpopulated'
+        clean.text = message
         clean.board = self.board
-        clean.name = "BOB"
-        clean.message = "BOB"
+        clean.player = self.player
+        clean.turn = self.turn
         return clean
-    def clone(self, data):
-        #TODO: Create function that can clone itself from a pickle
-    def start(self, server, port):
-        # Establish a connection to the server
+    def clone(data):
+        """
+        Creates a ClientState object from a pickle.
+
+        data: Pickled object.
+        """
+        clean = ClientState()
+        clean.new = data.new
+        clean.close = data.close
+        clean.over = data.over
+        clean.text = data.text
+        clean.board = data.board
+        clean.player = data.player
+        clean.turn = data.turn
+        return clean
+    def connect(self, server, port):
+        print('Establishing client connection.')
         self.client = stockings.SocketClient(server, int(port), 4096)
         self.client.connect()
-        self.listener = Process(target=self.client.start, args=(self.game,))
-        self.listener.start()
+    def listen(self):
+        print('Establing client listener.')
+        self.client.start(self.game)
+        print('Closing listener')
+    def start(self):
+        # Establish a connection to the server
         try:
-            print('Requesting new game session.')
-            self.client.send(pickle.dumps(self.copy('New')))
-        except KeyboardInterrupt:
+            self.client.send(pickle.dumps(self.message('New')))
+        except:
+            print('Closing connection.')
+            self.client.send(pickle.dumps(self.message('Close')))
             self.client.close()
             self.listener.join()
             return False
-        self.listener.join()
-    def game(self, data):
-        print('Client game')
-        if data.player.id = self.player.id:
-            #TODO: Your turn
-        else:
-            #TODO: wait for other player
-        pass
+    def game(self, pickle_dump):
+        data = pickle.loads(pickle_dump)
+        print('%s: %s says %s' % (str(datetime.datetime.utcnow()), data.player.id, data.text))
+        try:
+            if data.over:
+                print('Server overpopulated.')
+                return
+            else:
+                self.client.send(pickle.dumps(self.message(self.player.id)))
+        except Exception as e:
+            print(e)
